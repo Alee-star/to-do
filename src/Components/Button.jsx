3fb93df629b3189b5767f7b65cc44459a307e5cc
Alee-star <toaleenavshaji@gmail.com>
2024-11-22 19:44:19 +0530

    Update Button.jsx

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,13 @@ import React from "react"
 
 const Button = ({label, backgroundColor, onClick, isDisabled = false, isActive = false}) => {
     return (
-        <button onClick={onClick} style={{ backgroundColor: isActive ? "rgb(10, 177, 173)" : backgroundColor}} disabled={isDisabled}>{label}</button>
+        <button 
+            onClick={onClick} 
+            style={{ backgroundColor: isActive ? "rgb(10, 177, 173)" : backgroundColor}} 
+            disabled={isDisabled}
+        >
+        {label}
+        </button>
     )
 }
 export default Button
